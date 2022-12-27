b474bee3be3721c549f57a45f863854ab86530fe
Italo <italocampdantas@gmail.com>
2022-12-27 09:31:41 -0300

    Novo index

--- banco.php
+++ banco.php
@@ -1,12 +1,48 @@
+
+<?php include 'Usuario.class.php' ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Bem vindo!
-</body>
-</html>
+
+<html lang="pt-br">
+    <head>
+
+        <meta charset="UTF-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <meta name="viewport"content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="css/nstyle.css">
+
+        <title>bancoWiti</title>
+
+    </head>
+
+    <body>
+
+        <div id="login">
+
+            <form class="card" action="../bancoWiti/php/login.php" method="POST">
+
+                <div class="card-header">
+                <label for="usuario">Bem vindo ao banco Witi!!</label> 
+                    <h2><?php echo $dado['nome']?>bancoWiti</h2>
+                    
+                </div>
+
+                <div class="card-content">
+
+                    <div class="card-content-area">
+
+                       
+                        <div class="card-content-area">
+
+                <label for="usuario">Saldo</label>
+                        
+
+                    </div>
+
+            </form>
+
+        </div>
+
+    </body>
---
+++ novo_index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+    <head>
+
+        <meta charset="UTF-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <meta name="viewport"content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="css/nstyle.css">
+
+        <title>Login</title>
+
+    </head>
+
+    <body>
+
+        <div id="login">
+
+            <form class="card" action="../bancoWiti/php/login.php" method="POST">
+
+                <div class="card-header">
+
+                    <h2>Login</h2>
+
+                </div>
+
+                <div class="card-content">
+
+                    <div class="card-content-area">
+
+                        <label for="usuario">Usuário</label>
+
+                        <input type="text" name="email" id="usuario" autocomplete="off">
+
+                    </div>
+
+                    <div class="card-content-area">
+
+                        <label for="password">Senha</label>
+
+                        <input type="password" name="senha" id="password" autocomplete="off">
+
+                    </div>
+
+                </div>
+
+                <div class="card-footer">
+
+                    <input type="submit" value="login" class="submit">
+
+                    <a href="#" class="recuperar_senha">Esqueceu a senha?</a>
+
+                </div>
+
+            </form>
+
+        </div>
+
+    </body>
