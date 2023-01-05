d8addddc3997c5bf9fd891f8bc100d8e989c83d9
Italo <italocampdantas@gmail.com>
2023-01-05 17:43:55 -0300

    Alteração no index

--- index.php
+++ index.php
@@ -10,71 +10,72 @@ require 'php/login.php';
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login Witi Bank</title>
-   <!--Made with love by Mutiullah Samim -->
-   
-	<!--Bootsrap 4 CDN-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    
-    <!--Fontawesome CDN-->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<title>Login Witi Bank</title>
+<!--Made with love by Mutiullah Samim -->
 
-	<!--Custom styles-->
-	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+<!--Bootsrap 4 CDN-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<!--Fontawesome CDN-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+<!--Custom styles-->
+<link rel="stylesheet" type="text/css" href="css/estilo.css">
 </head>
 <body>
 <div class="container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Witi Bank</h3>
-				<div class="d-flex justify-content-end social_icon">
-					<span><i class="fab fa-facebook-square"></i></span>
-					<span><i class="fab fa-google-plus-square"></i></span>
-					<span><i class="fab fa-twitter-square"></i></span>
-				</div>
+<div class="d-flex justify-content-center h-100">
+	<div class="card">
+		<div class="card-header">
+			<h3>Witi Bank</h3>
+			<div class="d-flex justify-content-end social_icon">
+				<span><i class="fab fa-facebook-square"></i></span>
+				<span><i class="fab fa-google-plus-square"></i></span>
+				<span><i class="fab fa-twitter-square"></i></span>
 			</div>
-      <div class="card-body" style="color:aliceblue">
-        <?php 
-          if(isset($_SESSION['msg'])){
-            echo $_SESSION['msg'];
-            unset($_SESSION['msg']);
-          }
-        ?>
-      </div>
-			<div class="card-body">
-				<form action="../bancoWiti/php/login.php" method="POST">
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						<input type="text" class="form-control" placeholder="EMAIL" name="email" required>
-						
+		</div>
+	<div class="card-body" style="color:aliceblue">
+	<?php 
+		if(isset($_SESSION['msg'])){
+		echo $_SESSION['msg'];
+		unset($_SESSION['msg']);
+		}
+	?>
+	</div>
+		<div class="card-body">
+			<form action="../bancoWiti/php/login.php" method="POST">
+				<div class="input-group form-group">
+					<div class="input-group-prepend">
+						<span class="input-group-text"><i class="fas fa-user"></i></span>
 					</div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-key"></i></span>
-						</div>
-						<input type="password" class="form-control" placeholder="SENHA" name="senha" required>
+					<input type="text" class="form-control" placeholder="EMAIL" name="email" required>
+					
+				</div>
+				<div class="input-group form-group">
+					<div class="input-group-prepend">
+						<span class="input-group-text"><i class="fas fa-key"></i></span>
 					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox">Lembre-me
-					</div>
-					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
-					</div>
-				</form>
+					<input type="password" class="form-control" placeholder="SENHA" name="senha" required>
+				</div>
+				<div class="row align-items-center remember">
+					<input type="checkbox">Lembre-me
+				</div>
+				<div class="form-group">
+					<input type="submit" value="Login" class="btn float-right login_btn">
+				</div>
+			</form>
+		</div>
+		<div class="card-footer">
+			<div class="d-flex justify-content-center links">
+				Criar uma conta?<a href="#">Logar</a>
 			</div>
-			<div class="card-footer">
-				<div class="d-flex justify-content-center links">
-					Criar uma conta?<a href="#">Logar</a>
-				</div>
-				<div class="d-flex justify-content-center">
-					<a href="#">Esqueceu sua senha?</a>
-				</div>
+			<div class="d-flex justify-content-center">
+				<a href="#">Esqueceu sua senha?</a>
 			</div>
 		</div>
 	</div>
 </div>
+</div>
 </body>
 </html>
+<!------ fim ---------->
